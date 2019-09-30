4f07a0b59732f877957462cc8ebb3ff9256082c9
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-30 06:26:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,7 +12,6 @@ songs = [
 
 def help
   puts "I accept the following commands:"
-  puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
